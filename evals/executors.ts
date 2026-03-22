beda3b7965b2a26f31bc38ce18108e71daf99cf9
Anders Abjörn <anteabjorn@gmail.com>
2026-03-22 16:11:46 +0100

    Started to write mock and excecuters

--- evals/executors.ts
+++ evals/executors.ts
@@ -1,6 +1,24 @@
+import { generateText, stepCountIs, tool, type ToolSet }from "ai";
+import {openai} from "@ai-sdk/openai";
+import {z} from "zod";
+
 import type {
   EvalData,
   SingleTurnResult,
   MultiTurnEvalData,
   MultiTurnResult,
 } from "./types.ts";
+
+const TOOL_DEFINITIONS = {
+  readFile: {
+    description: "Read the content of a file at the specified path",
+    parameters: z.object({
+      path: z.string().describe('the path to the file you want to read'),
+    ){
+}
+  },
+  writefile: {},
+  listFiles: {},
+  deleteFile: {},
+  runCommand: {},
+}
