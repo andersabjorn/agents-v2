135c5870bbb064f7d636eee97ac22e91a42167e2
Anders Abjörn <anteabjorn@gmail.com>
2026-03-22 10:25:45 +0100

    Added Laminar, tracer and started evals

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { generateText, type ModelMessage } from "ai";
 import { openai } from "@ai-sdk/openai";
-
+import { getTracer, Laminar } from '@lmnr-ai/lmnr'
 import { tools } from "./tools/index.ts";
 import { SYSTEM_PROMPT } from "./system/prompt.ts";
 
@@ -9,23 +9,25 @@ import type { AgentCallbacks } from "../types.ts";
 
 const MODEL_NAME = "gpt-4o-mini";
 
+Laminar.initialize({
+    projectApiKey: process.env.LMNR_PROJECT_API_KEY,
+})
 export async function runAgent(
     userMessage: string,
     conversationHistory: ModelMessage[],
     callbacks: AgentCallbacks,
 ): Promise<any> {
     // Filter and check if we need to compact the conversation history before starting
-    const { text, toolCalls, toolResults } = await generateText({
+    const { text } = await generateText({
         model: openai(MODEL_NAME),
         prompt: userMessage,
         system: SYSTEM_PROMPT,
         tools,
-        maxSteps: 5,
+        experimental_telemetry: {
+            isEnabled: true,
+            tracer: getTracer(),
+        },
+        
     });
-// Starting evals
-    console.log("Text:", text);
-    console.log("Tool calls:", toolCalls);
-    console.log("Tool results:", toolResults);
 };
-
-runAgent("What is the current time right now?");
+console.log("done")
